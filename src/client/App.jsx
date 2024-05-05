977b8e0c2920fa11c065bc2a05c1f828c7c981d8
leosstheleo <p.ty8240@student.nu.edu>
2024-05-05 14:32:20 -0700

    FrontEnd

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -24,6 +24,7 @@ function App() {
     }
     getUser();
   }, [user]);
+  console.log(user)
   return (
     <div className="App">
       <Navbar />
--- src/client/components/AllCars.jsx
+++ src/client/components/AllCars.jsx
@@ -1,74 +1,19 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import { Link } from "react-router-dom";
+import React from "react";
+
+import SearchBar from "./Searchbar";
 
 function AllCars() {
-  const [search, setSearch] = useState("");
-  const [cars, setCars] = useState([]);
 
-  const filtered = cars.filter(
-    (car) =>
-      car.model.includes(search) ||
-      car.color.includes(search) ||
-      car.year.includes(search) ||
-      car.make.includes(search) ||
-      car.vehicleType.includes(search)
-  );
 
-  // useEffect(() => {
-  //   async function getCars() {
-  //     try {
-  //       const { data: foundCars } = await axios.get("/api/singlecar");
-  //       setCars(foundCars);
-
-  //       console.log(foundCars);
-  //     } catch (error) {
-  //       console.error(error);
-  //     }
-  //   }
-  //   getCars();
-  // }, []);
   return (
-    <div>
-      <h2>All Cars</h2>
-      <input
-        placeholder="search..."
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-      />
-      <button onClick={() => setSearch("")}>Submit</button>
-      {search.length === 0
-        ? cars.map((car) => (
-            <div key={car.id}>
-              <Link to={`/${car.id}`}>
-                <h3>Make: {car.make}</h3>
-                <h3>Model: {car.model}</h3>
-                <h3>Used/New: {car.new ? "Yes" : "No"}</h3>
-                <h3>Color: {car.color}</h3>
-                <h3>Year: {car.year}</h3>
-                <h3>Vehicle Type: {car.vehicleType}</h3>
-                <h3>Image: {car.img}</h3>
-                <h3>Price: {car.price}</h3>
-                <h3>Vin #: {car.vinNumber}</h3>
-              </Link>
-            </div>
-          ))
-        : filtered.map((car) => (
-            <div key={car.id}>
-              <Link to={`/${car.id}`}>
-                <h3>Make: {car.make}</h3>
-                <h3>Model: {car.model}</h3>
-                <h3>New: {car.new ? "Yes" : "No"}</h3>
-                <h3>Color: {car.color}</h3>
-                <h3>Year: {car.year}</h3>
-                <h3>Vehicle Type: {car.vehicleType}</h3>
-                <h3>Image: {car.img}</h3>
-                <h3>Price: {car.price}</h3>
-                <h3>Vin #: {car.vinNumber}</h3>
-              </Link>
-            </div>
-          ))}
+    <div className="herobg ">
+      <div className="heroContainer">
+<SearchBar/>
+      
+     
+     
     </div>
+              </div>
   );
 }
 
